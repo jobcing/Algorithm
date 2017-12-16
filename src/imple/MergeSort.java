b8fb2242117faf0778a26bdac2a9a392e8d39949
Cho ByeongChan <jobc0335@gmail.com>
2017-12-16 18:16:00 +0900

    merge sort in progress

--- src/imple/MergeSort.java
+++ src/imple/MergeSort.java
@@ -42,6 +42,41 @@ public class MergeSort {
     }
 
     public static void merge(int[] list, int L, int M, int R){
-        
+        int[] temp = list;
+
+        int i = L;
+        int j = M + 1;
+
+        int k = L;
+
+        while(i <= M && j <= R){
+            if(list[i] <= list[j]){
+                temp[k] = list[i++];
+            } else{
+                temp[k] = list[j++];
+            }
+
+            k++;
+        }
+
+        if(i > M){
+            for (int l = j; l <= R; l++, k++) {
+                temp[k] = list[l];
+            }
+        } else{
+            for (int l = i; l <= M; l++, k++) {
+                temp[k] = list[l];
+            }
+        }
+
+        for (int l = L; l <= R; l++) {
+            list[l] = temp[l];
+        }
+
+        System.out.println(">> ");
+        for (int l = 0; l < list.length; l++) {
+            System.out.print(list[l] + " ");
+        }
+        System.out.println();
     }
 }
