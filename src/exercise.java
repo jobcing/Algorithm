149076ec1ea376b92f83f63a6e7f69f65fe68966
Cho ByeongChan <jobc0335@gmail.com>
2017-12-18 19:24:21 +0900

    comparable exercise

--- src/exercise.java
+++ src/exercise.java
@@ -1,14 +1,34 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * Created by ByeongChan on 2017. 12. 4..
  */
 public class exercise {
-    private class Node{
-        private int data;
-        private Node prev;
-        private Node next;
+
+    public class Person implements Comparable<Person>{
+        private String name;
+        private int age;
+        private int point;
+
+        @Override
+        public int compareTo(Person p){
+            return this.age - p.age; // 오름차순 정렬
+
+            // 내림차순 정렬
+            // return p.age - this.age;
+        }
     }
 
-    private Node head;
-    private int size;
+    public static void main(String[] args) {
+        Person[] person = new Person[10];
+
+        Arrays.sort(person, new Comparator<Person>(){
+            public int compare(Person p1, Person p2){ return p1.point - p2.point; }
+        });
+
+        ArrayList
+    }
+
 
 }
