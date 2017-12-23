1d093b780dcd1892aafa357a7ef7a83bdec06360
Cho ByeongChan <jobc0335@gmail.com>
2017-12-23 17:52:05 +0900

    add boj_2079 problem

---
+++ src/imple/BOJ_2079.java
@@ -0,0 +1,46 @@
+package imple;
+
+import java.util.Scanner;
+
+/**
+ * Created by ByeongChan on 2017. 9. 21..
+ *
+ * 백준 알고리즘 2079번 풀이 (https://www.acmicpc.net/problem/2079)
+ *
+ * 팰린드롬(palindrome)
+ */
+public class BOJ_2079 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String inputStr = scan.nextLine();
+        int palinCnt = 0;
+
+        int start = 0;
+        int end = inputStr.length();
+
+        while(start < inputStr.length()){
+            if(!palinCheck(inputStr.substring(start, end))){
+                end--;
+            } else{
+                start = end;
+                end = inputStr.length();
+                palinCnt++;
+            }
+        }
+
+        System.out.println(palinCnt);
+    }
+
+    private static boolean palinCheck(String str){
+        int m = str.length() / 2;
+
+        for (int i = 0; i <= m; i++) {
+            if(str.charAt(i) != str.charAt(str.length() - 1 - i)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
