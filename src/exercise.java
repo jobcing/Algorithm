6ca5e824afb4ae3d3afafb3aa6edecc81167f8f1
Cho ByeongChan <jobc0335@gmail.com>
2018-01-18 17:13:33 +0900

    exercise boj_10610

--- src/exercise.java
+++ src/exercise.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -5,5 +8,31 @@ import java.util.Comparator;
  * Created by ByeongChan on 2017. 12. 4..
  */
 public class exercise {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
+        String n = br.readLine();
+        int[] nums = new int[10];
+        int sum = 0;
+
+        for (int i = 0; i < n.length(); i++) {
+            int num = n.charAt(i) - '0';
+            nums[num]++;
+            sum += num;
+        }
+
+        if(sum % 3 != 0 || nums[0] == 0){
+            System.out.println(-1);
+            return;
+        }
+
+        for (int i = 9; i >= 0; i--) {
+            while(nums[i]-- > 0){
+                sb.append(i);
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
 }
