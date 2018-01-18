85cd67754b1901318e6f28a7a1a47c190c315c45
Cho ByeongChan <jobc0335@gmail.com>
2018-01-18 15:15:49 +0900

    init exercise

--- src/exercise.java
+++ src/exercise.java
@@ -6,27 +6,4 @@ import java.util.Comparator;
  */
 public class exercise {
 
-    public class Person implements Comparable<Person>{
-        private String name;
-        private int age;
-        private int point;
-
-        @Override
-        public int compareTo(Person p){
-            return this.age - p.age; // 오름차순 정렬
-
-            // 내림차순 정렬
-            // return p.age - this.age;
-        }
-    }
-
-    public static void main(String[] args) {
-        Person[] person = new Person[10];
-
-        Arrays.sort(person, new Comparator<Person>(){
-            public int compare(Person p1, Person p2){ return p1.point - p2.point; }
-        });
-    }
-
-
 }
