f2ea00d05b5d3b92a38bac3ec66847226fd7bc4d
Cho ByeongChan <jobc0335@gmail.com>
2018-03-12 16:43:16 +0900

    add boj_2743 solution

---
+++ src/imple/BOJ_11654.java
@@ -0,0 +1,7 @@
+package imple;
+
+/**
+ * Created by ByeongChan on 2018. 3. 12..
+ */
+public class BOJ_11654 {
+}
---
+++ src/imple/BOJ_2743.java
@@ -0,0 +1,19 @@
+package imple;
+
+import java.util.Scanner;
+
+/**
+ * Created by ByeongChan on 2018. 3. 12..
+ *
+ * 백준 알고리즘 2743번 풀이 (https://www.acmicpc.net/problem/2743)
+ */
+
+public class BOJ_2743 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String inputStr = scan.nextLine();
+
+        System.out.println(inputStr.length());
+    }
+}
