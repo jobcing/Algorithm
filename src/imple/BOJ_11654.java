42a4c2d32983c56ec03623ef61fbc24aee737d1c
Cho ByeongChan <jobc0335@gmail.com>
2018-03-12 16:45:49 +0900

    add boj_11654 solution

--- src/imple/BOJ_11654.java
+++ src/imple/BOJ_11654.java
@@ -1,7 +1,19 @@
 package imple;
 
+import java.util.Scanner;
+
 /**
  * Created by ByeongChan on 2018. 3. 12..
+ *
+ * 백준 알고리즘 11654번 풀이 (https://www.acmicpc.net/problem/11654)
  */
+
 public class BOJ_11654 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        char c = scan.nextLine().charAt(0);
+
+        System.out.println((int) c);
+    }
 }
