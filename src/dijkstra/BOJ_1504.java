00fcf0b2e588455a710f122c072d6be0b3b2c847
Cho ByeongChan <jobc0335@gmail.com>
2018-03-18 21:49:12 +0900

    modify boj_1504 solution

--- src/dijkstra/BOJ_1504.java
+++ src/dijkstra/BOJ_1504.java
@@ -3,8 +3,6 @@ package dijkstra;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
@@ -16,74 +14,76 @@ import java.util.StringTokenizer;
  */
 
 public class BOJ_1504 {
+    // private static final int INF = Integer.MAX_VALUE;
 
-    private static final int INF = Integer.MAX_VALUE;
+    private static final int INF = 100000;
 
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    private static int N, E;
+    private static int[][] weight;
+    private static int[][] dist;
 
-        int N = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-
-        // ArrayList<ArrayList<Node>> weight = new ArrayList<>();
-        int[][] weight = new int[N + 1][N + 1];
-        int[][] dist = new int[3][N + 1];
-//
-//        for (int i = 0; i <= N; i++) {
-//            weight.add(new ArrayList<Node>());
-//        }
-
+    static {
         for (int i = 0; i < 3; i++) {
+
             Arrays.fill(dist[i], INF);
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
 
         for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+
+            st = new StringTokenizer(br.readLine());
 
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-//            weight.get(a).add(new Node(b, c));
-//            weight.get(b).add(new Node(a, c));
             weight[a][b] = c;
             weight[b][a] = c;
         }
 
-        st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
         int node1 = Integer.parseInt(st.nextToken());
         int node2 = Integer.parseInt(st.nextToken());
 
-        dijkstra(weight, dist, N, 1, 0);
-        dijkstra(weight, dist, N, node1, 1);
-        dijkstra(weight, dist, N, node2, 2);
+        // 1번, node1, node2에서 각각 최단거리를 구해 dist[idx]에 순서대로 저장한다.
+        dijkstra(1, 0);
+        dijkstra(node1, 1);
+        dijkstra(node2, 2);
 
-        int minCost = Math.min(dist[0][node1] + dist[1][node2] + dist[2][N],
-                dist[0][node2] + dist[2][node1] + dist[1][N]);
+        // 1번 -> node1 -> node2 -> N
+        // 1번 -> node2 -> node1 -> N
+        // 두 가지 경우의 수 중에 최소값을 택함
+        int minCost = Math.min(dist[0][node1] + dist[1][node2] + dist[2][N], dist[0][node2] + dist[2][node1] + dist[1][N]);
 
         System.out.println(minCost >= INF ? -1 : minCost);
     }
 
-    private static void dijkstra(int[][] weight, int[][] dist, int N, int S, int idx){
+    private static void dijkstra(int S, int idx) {
+
         PriorityQueue<Node> pq = new PriorityQueue<>();
 
-        pq.offer(new Node(S, 0));
+        pq.add(new Node(S, 0));
         dist[idx][S] = 0;
 
-        while(!pq.isEmpty()){
+        while (!pq.isEmpty()) {
+
             Node u = pq.poll();
 
-            if(dist[idx][u.node] < u.dist) continue;
+            // 이미 최소값이라면 넘어간다.
+            if (dist[idx][u.node] < u.dist) continue;
 
-//            for (Node adjNode : list.get(u.node)){
-//                if(dist[idx][adjNode.node] > adjNode.dist + u.dist){
-//                    dist[idx][adjNode.node] = adjNode.dist + u.dist;
-//                    pq.offer(new Node(adjNode.node, dist[idx][adjNode.node]));
-//                }
-//            }
             for (int i = 1; i <= N; i++) {
-                if(weight[u.node][i] != 0){
+
+                if (weight[u.node][i] != 0) {
+
                     if (dist[idx][i] > dist[idx][u.node] + weight[u.node][i]) {
 
                         dist[idx][i] = dist[idx][u.node] + weight[u.node][i];
@@ -98,7 +98,7 @@ public class BOJ_1504 {
         int node;
         int dist;
 
-        public Node(int node, int dist){
+        public Node(int node, int dist) {
             this.node = node;
             this.dist = dist;
         }
