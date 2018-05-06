ab17a612c738ed5feacbd6e7543057f81d0d61e7
Cho ByeongChan <jobc0335@gmail.com>
2018-05-06 17:10:50 +0900

    add boj_10172 solution

---
+++ src/imple/BOJ_10172.java
@@ -0,0 +1,23 @@
+package imple;
+
+import java.io.IOException;
+
+/**
+ * Created by ByeongChan on 2018. 5. 6..
+ *
+ * 백준 알고리즘 10172번 풀이 (https://www.acmicpc.net/problem/10172)
+ */
+
+public class BOJ_10172 {
+    public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+        sb.append("|\\_/|\n");
+        sb.append("|q p|   /}\n");
+        sb.append("( 0 )\"\"\"\\\n");
+        sb.append("|\"^\"`    |\n");
+        sb.append("||_/=\\\\__|");
+
+        // 결과 값 출력
+        System.out.println(sb.toString());
+    }
+}
